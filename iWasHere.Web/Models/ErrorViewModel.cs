c3b601abc14a5459d131524f2813ffa68298a97f
Diana Vasile <dvasile@totalsoft.ro>
2019-09-12 12:20:25 +0300

    Test Diana

--- iWasHere.Web/Models/ErrorViewModel.cs
+++ iWasHere.Web/Models/ErrorViewModel.cs
@@ -4,7 +4,7 @@ namespace iWasHere.Web.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public string RequestId { get; set; } //test
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
