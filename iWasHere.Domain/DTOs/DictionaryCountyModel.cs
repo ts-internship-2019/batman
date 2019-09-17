2b00f56095083bbc8fa2e8cf22ae0f42dea71c1c
Diana Vasile <dvasile@totalsoft.ro>
2019-09-17 16:41:43 +0300

    TI 109 Counties Autocomplete rezolvat + paginare

---
+++ iWasHere.Domain/DTOs/DictionaryCountyModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iWasHere.Domain.DTOs
+{
+   public class DictionaryCountyModel
+    {
+        public int CountyId { get; set; }
+        public string CountyName { get; set; }
+        public string CountyCode { get; set; }        
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }
+    }
+}
--- iWasHere.Web/Controllers/DictionaryController.cs
+++ iWasHere.Web/Controllers/DictionaryController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using iWasHere.Domain.DTOs;
 using iWasHere.Domain.Service;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
 using Microsoft.AspNetCore.Mvc;
 
 namespace iWasHere.Web.Controllers
@@ -22,6 +24,42 @@ namespace iWasHere.Web.Controllers
             List<DictionaryLandmarkTypeModel> dictionaryLandmarkTypeModels = _dictionaryService.GetDictionaryLandmarkTypeModels();
 
             return View(dictionaryLandmarkTypeModels);
+        }
+
+        public IActionResult Counties()
+        {
+            // List<DictionaryCountyModel> dictionaryCountyModels = _dictionaryService.GetDictionaryCountyModels();
+
+            // return View(dictionaryCountyModels);
+
+            return View();
+        }
+
+        public ActionResult CountyData([DataSourceRequest]DataSourceRequest request)
+        {
+            List<DictionaryCountyModel> data = _dictionaryService.GetDictionaryCountyModels(request.Page, request.PageSize);
+            var result = new DataSourceResult()
+            {
+                Data = data, // process data (paging and sorting applied)
+                Total = _dictionaryService.GetDictionaryCountyCount()
+
+            };
+            return Json(result);
+        }
+
+        public ActionResult FilterCountyData([DataSourceRequest]DataSourceRequest request, string searchCountyName, string searchCountyCode, string searchCountryName)
+        {
+            return Json(_dictionaryService.FilterDictionaryCountyModels(searchCountyName, searchCountyCode, searchCountryName, request.Page, request.PageSize).ToDataSourceResult(request));
+        }
+
+        public ActionResult GetCountryList([DataSourceRequest]DataSourceRequest request)
+        {
+            return Json(_dictionaryService.GetCountryList());
+        }
+
+        public ActionResult ServerFiltering_GetCountries(string text)
+        {
+            return Json(_dictionaryService.ServerFiltering_GetCountries(text));
         }
     }
 }
