1372e5c95155b9b2ca4be4b893552245a5a4572f
Gabriela Bucur <gbucur@totalsoft.ro>
2019-09-17 17:19:38 +0300

    TI_105

--- iWasHere.Web/Controllers/DictionaryController.cs
+++ iWasHere.Web/Controllers/DictionaryController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using iWasHere.Domain.DTOs;
+using iWasHere.Domain.Model;
 using iWasHere.Domain.Service;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace iWasHere.Web.Controllers
 {
     public class DictionaryController : Controller
     {
         private readonly DictionaryService _dictionaryService;
+
+
+        public object SeasonName { get; private set; }
 
         public DictionaryController(DictionaryService dictionaryService)
         {
@@ -23,5 +30,26 @@ namespace iWasHere.Web.Controllers
 
             return View(dictionaryLandmarkTypeModels);
         }
+
+
+        public ActionResult DictionarySeasonTypeData([DataSourceRequest]DataSourceRequest request)
+        {
+            return Json(_dictionaryService.GetDictionarySeasonTypeModels(request.Page, request.PageSize).ToDataSourceResult(request));
+        }
+
+        public IActionResult DictionarySeasonType()
+        {
+            return View();
+        }
+
+      
+
+
+
+
+
+
+
+
     }
 }
