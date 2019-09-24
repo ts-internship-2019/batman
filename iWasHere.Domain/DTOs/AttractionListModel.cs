091be73ee2a9703a8f99ffa1d4487432ec4db8a6
Alex Sara <asara@totalsoft.ro>
2019-09-24 12:57:44 +0300

    list

---
+++ iWasHere.Domain/DTOs/AttractionListModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iWasHere.Domain.DTOs
+{
+    public class AttractionListModel
+    {
+    }
+}
---
+++ iWasHere.Web/Controllers/AttractionController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iWasHere.Web.Controllers
+{
+    public class AttractionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
