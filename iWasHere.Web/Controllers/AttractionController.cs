f52ed96ed9030da7d4372cf0c617aa288389ecd1
Alex Sara <asara@totalsoft.ro>
2019-09-24 13:50:04 +0300

    list attractions

--- iWasHere.Web/Controllers/AttractionController.cs
+++ iWasHere.Web/Controllers/AttractionController.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using iWasHere.Domain.Service;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
 using Microsoft.AspNetCore.Mvc;
 
 namespace iWasHere.Web.Controllers
 {
     public class AttractionController : Controller
     {
+        private readonly AtractionService _atractionService;
+        public AttractionController(AtractionService atractionService)
+        {
+            _atractionService = atractionService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult GetAttractions([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(_atractionService.GetAttractionListModels().ToDataSourceResult(request));
         }
     }
 }
