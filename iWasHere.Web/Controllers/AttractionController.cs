c5aabe27d63ee8176197219c59e3f87ebcf62139
Alex Sara <asara@totalsoft.ro>
2019-09-24 13:53:59 +0300

    a

--- iWasHere.Web/Controllers/AttractionController.cs
+++ iWasHere.Web/Controllers/AttractionController.cs
@@ -1,28 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using iWasHere.Domain.Service;
+﻿using iWasHere.Domain.Service;
 using Kendo.Mvc.Extensions;
 using Kendo.Mvc.UI;
 using Microsoft.AspNetCore.Mvc;
 using iWasHere.Domain.DTOs;
-using iWasHere.Domain.Model;
-using iWasHere.Domain.Service;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace iWasHere.Web.Controllers
 {
     public class AttractionController : Controller
     {
-        private readonly AtractionService _atractionService;
+        private readonly AtractionService _attractionService;
         public AttractionController(AtractionService atractionService)
         {
-            _atractionService = atractionService;
+            _attractionService = atractionService;
+        }
+
+        public ActionResult Attraction(int attrId)
+        {
+            AttractionModel attr = new AttractionModel();
+            attr = _attractionService.GetAttractionLocation(attrId);
+            return View(attr);
         }
 
         public IActionResult Index()
@@ -32,29 +29,7 @@ namespace iWasHere.Web.Controllers
 
         public IActionResult GetAttractions([DataSourceRequest] DataSourceRequest request)
         {
-            return Json(_atractionService.GetAttractionListModels().ToDataSourceResult(request));
-        }
-    }
-}
-
-namespace iWasHere.Web.Controllers
-{
-    public class AttractionController : Controller
-    {
-        private readonly AttractionService _attractionService;
-        private readonly DatabaseContext _dbContext;
-
-        public AttractionController(AttractionService attractionService, DatabaseContext databaseContext)
-        {
-            _attractionService = attractionService;
-            _dbContext = databaseContext;
-        }       
-
-        public ActionResult Attraction(int attrId)
-        {
-            AttractionModel attr = new AttractionModel();
-            attr = _attractionService.GetAttractionLocation(attrId);
-            return View(attr);
+            return Json(_attractionService.GetAttractionListModels().ToDataSourceResult(request));
         }
     }
 }
